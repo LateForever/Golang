b7637632e055b7522e09cca83e87150a60db9d13
LateForever <albertgrzegrzolka124@gmail.com>
2023-04-11 01:00:03 +0200

    added basics of lesson 4

--- lessons/@Lesson #03/interfaces.go
+++ lessons/@Lesson #03/interfaces.go
@@ -1,1 +1,7 @@
 package lessons03
+
+//@ Interface
+// In short, an interface is a set of methods that we use to define a set of actions
+// An interface defines a set of methods, so if a type implements all the methods we say that it implements the interface
+
+
